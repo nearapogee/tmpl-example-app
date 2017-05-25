3913ec10d99b57278c18972f2578752581724ac4
Matt Smith <matt@nearapogee.com>
2017-05-25 09:04:30 -0600

    upgrade to rails 5.0.2

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 
-gem 'rails',        '4.0.2'
+gem 'rails',        '5.0.2'
 gem 'sqlite3'
+gem 'puma'
 
-gem 'sass-rails',   '~> 4.0.0'
+gem 'sass-rails',   '~> 5.0.0'
 gem 'uglifier',     '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.2'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,21 +12,21 @@
 
 ActiveRecord::Schema.define(version: 20140215191528) do
 
-  create_table "books", force: true do |t|
+  create_table "books", force: :cascade do |t|
     t.string   "name"
     t.string   "author"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "chapters", force: true do |t|
+  create_table "chapters", force: :cascade do |t|
     t.string   "title"
     t.integer  "book_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "headings", force: true do |t|
+  create_table "headings", force: :cascade do |t|
     t.string   "text"
     t.integer  "chapter_id"
     t.datetime "created_at"
